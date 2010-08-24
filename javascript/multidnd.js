73e26f25b620ec790ccfba2a6bc143635f2cffa5
David Ibbitson <david.ibbitson@gmail.com>
2010-08-23 22:09:17 -0700

    disabled ui selection lasso functionality. modified selection state handling to enforce the restriction of selection only when mouse down/up occur within the same element.

--- javascript/multidnd.js
+++ javascript/multidnd.js
@@ -18,7 +18,7 @@
   $.widget("ui.multidnd", $.ui.mouse, {
 
   	options: {
-  		appendTo: 'body',
+//  		appendTo: 'body',
   		autoRefresh: true,
   		distance: 0,
   		filter: '*',
@@ -58,7 +58,7 @@
 
   		this._mouseInit();
 
-  		this.helper = $("<div class='ui-selectable-helper'></div>");
+      // this.helper = $("<div class='ui-selectable-helper'></div>");
   	},
 
   	destroy: function() {
@@ -88,15 +88,15 @@
 
   		this._trigger("start", event);
 
-  		$(options.appendTo).append(this.helper);
-  		// position helper (lasso)
-  		this.helper.css({
-  			"left": event.clientX,
-  			"top": event.clientY,
-  			"width": 0,
-  			"height": 0
-  		});
-
+      // $(options.appendTo).append(this.helper);
+      //      // position helper (lasso)
+      //      this.helper.css({
+      //        "left": event.clientX,
+      //        "top": event.clientY,
+      //        "width": 0,
+      //        "height": 0
+      //      });
+  
   		if (options.autoRefresh) {
   			this.refresh();
   		}
@@ -145,83 +145,83 @@
   	_mouseDrag: function(event) {
   		var self = this;
   		this.dragged = true;
-
-  		if (this.options.disabled)
-  			return;
-
-  		var options = this.options;
-
-  		var x1 = this.opos[0], y1 = this.opos[1], x2 = event.pageX, y2 = event.pageY;
-  		if (x1 > x2) { var tmp = x2; x2 = x1; x1 = tmp; }
-  		if (y1 > y2) { var tmp = y2; y2 = y1; y1 = tmp; }
-  		this.helper.css({left: x1, top: y1, width: x2-x1, height: y2-y1});
-
-  		this.selectees.each(function() {
-  			var selectee = $.data(this, "selectable-item");
-  			//prevent helper from being selected if appendTo: selectable
-  			if (!selectee || selectee.element == self.element[0])
-  				return;
-  			var hit = false;
-  			if (options.tolerance == 'touch') {
-  				hit = ( !(selectee.left > x2 || selectee.right < x1 || selectee.top > y2 || selectee.bottom < y1) );
-  			} else if (options.tolerance == 'fit') {
-  				hit = (selectee.left > x1 && selectee.right < x2 && selectee.top > y1 && selectee.bottom < y2);
-  			}
-
-  			if (hit) {
-  				// SELECT
-  				if (selectee.selected) {
-  					selectee.$element.removeClass('ui-selected');
-  					selectee.selected = false;
-  				}
-  				if (selectee.unselecting) {
-  					selectee.$element.removeClass('ui-unselecting');
-  					selectee.unselecting = false;
-  				}
-  				if (!selectee.selecting) {
-  					selectee.$element.addClass('ui-selecting');
-  					selectee.selecting = true;
-  					// selectable SELECTING callback
-  					self._trigger("selecting", event, {
-  						selecting: selectee.element
-  					});
-  				}
-  			} else {
-  				// UNSELECT
-  				if (selectee.selecting) {
-  					if (event.metaKey && selectee.startselected) {
-  						selectee.$element.removeClass('ui-selecting');
-  						selectee.selecting = false;
-  						selectee.$element.addClass('ui-selected');
-  						selectee.selected = true;
-  					} else {
-  						selectee.$element.removeClass('ui-selecting');
-  						selectee.selecting = false;
-  						if (selectee.startselected) {
-  							selectee.$element.addClass('ui-unselecting');
-  							selectee.unselecting = true;
-  						}
-  						// selectable UNSELECTING callback
-  						self._trigger("unselecting", event, {
-  							unselecting: selectee.element
-  						});
-  					}
-  				}
-  				if (selectee.selected) {
-  					if (!event.metaKey && !selectee.startselected) {
-  						selectee.$element.removeClass('ui-selected');
-  						selectee.selected = false;
-
-  						selectee.$element.addClass('ui-unselecting');
-  						selectee.unselecting = true;
-  						// selectable UNSELECTING callback
-  						self._trigger("unselecting", event, {
-  							unselecting: selectee.element
-  						});
-  					}
-  				}
-  			}
-  		});
+      // 
+      // if (this.options.disabled)
+      //  return;
+      // 
+      // var options = this.options;
+      // 
+      // var x1 = this.opos[0], y1 = this.opos[1], x2 = event.pageX, y2 = event.pageY;
+      // if (x1 > x2) { var tmp = x2; x2 = x1; x1 = tmp; }
+      // if (y1 > y2) { var tmp = y2; y2 = y1; y1 = tmp; }
+      // this.helper.css({left: x1, top: y1, width: x2-x1, height: y2-y1});
+      // 
+      // this.selectees.each(function() {
+      //  var selectee = $.data(this, "selectable-item");
+      //  //prevent helper from being selected if appendTo: selectable
+      //  if (!selectee || selectee.element == self.element[0])
+      //    return;
+      //  var hit = false;
+      //  if (options.tolerance == 'touch') {
+      //    hit = ( !(selectee.left > x2 || selectee.right < x1 || selectee.top > y2 || selectee.bottom < y1) );
+      //  } else if (options.tolerance == 'fit') {
+      //    hit = (selectee.left > x1 && selectee.right < x2 && selectee.top > y1 && selectee.bottom < y2);
+      //  }
+      // 
+      //  if (hit) {
+      //    // SELECT
+      //    if (selectee.selected) {
+      //      selectee.$element.removeClass('ui-selected');
+      //      selectee.selected = false;
+      //    }
+      //    if (selectee.unselecting) {
+      //      selectee.$element.removeClass('ui-unselecting');
+      //      selectee.unselecting = false;
+      //    }
+      //    if (!selectee.selecting) {
+      //      selectee.$element.addClass('ui-selecting');
+      //      selectee.selecting = true;
+      //      // selectable SELECTING callback
+      //      self._trigger("selecting", event, {
+      //        selecting: selectee.element
+      //      });
+      //    }
+      //  } else {
+      //    // UNSELECT
+      //    if (selectee.selecting) {
+      //      if (event.metaKey && selectee.startselected) {
+      //        selectee.$element.removeClass('ui-selecting');
+      //        selectee.selecting = false;
+      //        selectee.$element.addClass('ui-selected');
+      //        selectee.selected = true;
+      //      } else {
+      //        selectee.$element.removeClass('ui-selecting');
+      //        selectee.selecting = false;
+      //        if (selectee.startselected) {
+      //          selectee.$element.addClass('ui-unselecting');
+      //          selectee.unselecting = true;
+      //        }
+      //        // selectable UNSELECTING callback
+      //        self._trigger("unselecting", event, {
+      //          unselecting: selectee.element
+      //        });
+      //      }
+      //    }
+      //    if (selectee.selected) {
+      //      if (!event.metaKey && !selectee.startselected) {
+      //        selectee.$element.removeClass('ui-selected');
+      //        selectee.selected = false;
+      // 
+      //        selectee.$element.addClass('ui-unselecting');
+      //        selectee.unselecting = true;
+      //        // selectable UNSELECTING callback
+      //        self._trigger("unselecting", event, {
+      //          unselecting: selectee.element
+      //        });
+      //      }
+      //    }
+      //  }
+      //       });
 
   		return false;
   	},
@@ -244,17 +244,25 @@
   		});
   		$('.ui-selecting', this.element[0]).each(function() {
   			var selectee = $.data(this, "selectable-item");
-  			selectee.$element.removeClass('ui-selecting').addClass('ui-selected');
-  			selectee.selecting = false;
-  			selectee.selected = true;
-  			selectee.startselected = true;
-  			self._trigger("selected", event, {
-  				selected: selectee.element
-  			});
+  			console.log($(event.target));
+  			console.log(selectee.$element);
+  			if( $(event.target)[0] == selectee.$element[0] ) {
+  			  selectee.$element.removeClass('ui-selecting').addClass('ui-selected');
+  			  selectee.selecting = false;
+  			  selectee.selected = true;
+  			  selectee.startselected = true;
+  			  self._trigger("selected", event, {
+  				  selected: selectee.element
+  			  });
+			  }
+			  else {
+			    selectee.$element.removeClass('ui-selecting');
+			    selectee.selecting = false;
+			  }
   		});
   		this._trigger("stop", event);
 
-  		this.helper.remove();
+      // this.helper.remove();
 
   		return false;
   	}
